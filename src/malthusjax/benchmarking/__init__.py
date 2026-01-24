67f4aed670b9d4d330f6d69e58323c3f57af1fcc
LeonardoDiCaterina <20240485@novaims.unl.pt>
2026-01-24 10:46:46 +0100

    feat(benchmarking): add IO helpers + tests

--- src/malthusjax/benchmarking/__init__.py
+++ src/malthusjax/benchmarking/__init__.py
@@ -3,6 +3,10 @@
 This package contains dataclasses and IO helpers used by the BenchmarkRunner.
 """
 
-from .results import RunResult, ExperimentResult
+from .io import ensure_seed_folder, read_summary_json, write_histories_csv, write_summary_json
+from .results import ExperimentResult, RunResult
 
-__all__ = ["RunResult", "ExperimentResult"]
+__all__ = [
+    "RunResult", "ExperimentResult",
+    "write_summary_json", "read_summary_json", "write_histories_csv", "ensure_seed_folder"
+]
