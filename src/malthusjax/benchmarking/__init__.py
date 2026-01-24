aaaabe01b827a10756df9cb1b8229ec679c6b3ea
LeonardoDiCaterina <20240485@novaims.unl.pt>
2026-01-24 10:21:16 +0100

    feat(benchmarking): add results dataclasses + tests

---
+++ src/malthusjax/benchmarking/__init__.py
@@ -0,0 +1,8 @@
+"""Benchmarking helpers for MalthusJAX.
+
+This package contains dataclasses and IO helpers used by the BenchmarkRunner.
+"""
+
+from .results import RunResult, ExperimentResult
+
+__all__ = ["RunResult", "ExperimentResult"]
