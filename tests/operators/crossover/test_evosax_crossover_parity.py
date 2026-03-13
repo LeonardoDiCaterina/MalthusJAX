895881e0afb79f136f1e66394eb97199045886ec
LeonardoDiCaterina <20240485@novaims.unl.pt>
2026-03-13 16:19:32 +0100

    Refactor import statement for evosax crossover to use the updated module path

--- tests/operators/crossover/test_evosax_crossover_parity.py
+++ tests/operators/crossover/test_evosax_crossover_parity.py
@@ -2,7 +2,8 @@ import jax
 import jax.numpy as jnp
 import jax.random as jar
 
-from malthusjax.compat.evosax_mimic import crossover as evosax_crossover
+#from malthusjax.compat.evosax_mimic import crossover as evosax_crossover
+from evosax.algorithms.population_based.simple_ga import crossover as evosax_crossover
 from malthusjax.core.genome.real_genome import RealGenome, RealGenomeConfig, RealPopulation
 from malthusjax.operators.crossover.evosax_crossover import EvosaxUniformCrossoverWrapper
 
