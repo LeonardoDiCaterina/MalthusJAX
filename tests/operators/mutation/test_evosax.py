a01e0f3d497e4abee260c57e43a223934ca90801
LeonardoDiCaterina <20240485@novaims.unl.pt>
2026-03-13 16:19:26 +0100

    Refactor import statements in mutation tests to use evosax directly

--- tests/operators/mutation/test_evosax.py
+++ tests/operators/mutation/test_evosax.py
@@ -4,7 +4,8 @@ import jax.random as jr
 import numpy as np
 import pytest
 
-from malthusjax.compat.evosax_mimic import mutation as evosax_func
+from evosax.algorithms.population_based.simple_ga import mutation as evosax_func
+#from malthusjax.compat.evosax_mimic import mutation as evosax_func
 from malthusjax.core.genome.real_genome import RealGenomeConfig, RealPopulation
 from malthusjax.operators.mutation.evosax_mutation import (
     EvosaxGaussianWrapper,
